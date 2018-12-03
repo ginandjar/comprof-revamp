fc04dbf201eaa177a704e6e6d7dfbca7d3ed7056
Adji Ginandjar L <adji.lazuardi@gvmnetworks.com>
2018-12-03 09:00:32 +0700

    research detail

--- pages/researchdetail.tsx
+++ pages/researchdetail.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 import Intro from '../components/layout/Intro';
 import HeroCard from '../components/layout/HeroCard';
 import ButtonLink from '../components/links/ButtonLink';
-
+import Fade from 'react-reveal/Fade';
 interface Props {
 
 }
@@ -39,23 +39,27 @@ class ResearchDetail extends React.Component<Props> {
         </Head>
         <NavigationBar localeService={this.props.localeService} />
         <div className="researchdetail">
+
           <div className="researchdetail__title">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
+            Empirical Evaluation of Character-Based Model on Neural Named-Entity Recognition in Indonesian Conversational Texts
           </div>
           <div className="researchdetail__descr">
-            <p>Publication year: 2018</p>
-            <p>Writen by: Kemal Maulana</p>
+            <Fade>
+              <p>Publication year: 2018</p>
+              <p>Kemal Kurniawan & Samuel Louvan</p>
+            </Fade>
             <hr />
           </div>
           <HeroCard title="Abstract" className="researchdetail__abstract">
             <p className="researchdetail__abstract__detail">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Despite the long history of named-entity recognition (NER) task in the natural language processing community, previous work rarely studied the task on conversational texts. Such texts are challenging because they contain a lot of word variations which increase the number of out-of-vocabulary (OOV) words. The high number of OOV words poses a difficulty for word-based neural models. Meanwhile, there is plenty of evidence to the effectiveness of character-based neural models in mitigating this OOV problem. We report an empirical evaluation of neural sequence labeling models with character embedding to tackle NER task in Indonesian conversational texts. Our experiments show that (1) character models outperform word embedding-only models by up to 4 $F_1$ points, (2) character models perform better in OOV cases with an improvement of as high as 15 $F_1$ points, and (3) character models are robust against a very high OOV rate.
             </p>
-            <ButtonLink modifier="default" href="/enterprise">
+            <ButtonLink modifier="default" href="https://noisy-text.github.io/2018/pdf/W-NUT201812.pdf ">
               Download Full Paper
                 {/* <img src="/static/images/icons/arrow-down-light.svg" /> */}
             </ButtonLink>
           </HeroCard>
+
           <HeroCard title="Related Content" className="researchdetail__related-content">
             <div className="researchdetail__related-content-list">
               <div className="researchdetail__related-content-list__title">
@@ -81,7 +85,7 @@ class ResearchDetail extends React.Component<Props> {
 
 
 
-      </div>
+      </div >
 
     );
   }
